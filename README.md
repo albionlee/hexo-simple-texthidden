f9a8ab048ba67b854f175168d6322f19eb305e1a
albionlee <57150069@qq.com>
2022-12-11 15:15:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## hexo-simple-texthidden
+hexo-simple-texthidden is a hexo plugin that hides text. It currently only supports the hiding of text in pure text format.
+
+## Install
+```
+npm install hexo-simple-texthidden --save
+```
+## Usage
+You can use it in the following way
+```
+{% textHidden color %}
+Hidden text
+{% endtextHidden %}
+```
+**color:  black、red、blue...**
+## End
+**Enjoy it ! **
---
+++ index.js
@@ -0,0 +1,17 @@
+hexo.extend.tag.register('textHidden', function(args, content){
+	// let text = content;
+	let fontColor = args[0];
+  return `
+   <style>
+    span.hidden-text {
+        color: black;
+        background-color: black;
+    }
+    span.hidden-text:hover {
+        color: while;
+        background-color: transparent;
+    }
+   </style>
+      <span class="hidden-text" onmouseover="this.style.color='${fontColor}'; this.style.backgroundColor='transparent';" onmouseout="this.style.color='black'; this.style.backgroundColor='black';">${content}</span>
+   `
+}, {ends: true, async: true})
